a2ad1b9f3c9b13c653562f69bc9b358e22d18e1b
Noel Treese <noeltreese@gmail.com>
2023-01-30 13:03:17 +0100

    fixed game menu button. Added flashlight noise

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light lightsource;
+
+    public InputActionProperty showButton;
+
+    void Update()
+    {
+        if (showButton.action.WasPressedThisFrame())
+        {
+            //lightsource.SetActive(!lightsource.activeSelf);
+            lightsource.enabled = !lightsource.enabled;
+        }
+    }
+}
--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
+/*Code sourced from Valem Tutorials on YouTube*/
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject gameMenu;
+    public GameObject flashLight;
     public InputActionProperty showButton;
 
     void Update()
@@ -14,10 +16,7 @@ public class GameMenu : MonoBehaviour
         if(showButton.action.WasPressedThisFrame())
         {
             gameMenu.SetActive(!gameMenu.activeSelf);
-
+            flashLight.SetActive(!gameMenu.activeSelf);
         }
-
     }
-
-    
 }
